f39d582969a06625c382782fd8f0efa421b8b2a1
Miguel-Galrito <mapgalrito@gmail.com>
2026-08-28 13:36:06 +0100

    Add physical constants

---
+++ src/orbit_simulator/domain/constants.py
@@ -0,0 +1,8 @@
+"""Physical constants used by the simulator."""
+
+G = 6.67430e-11  # m^3 kg^-1 s^-2
+EARTH_MASS = 5.97219e24  # kg
+EARTH_RADIUS = 6_378_137.0  # m (equatorial)
+EARTH_MU = G * EARTH_MASS  # m^3 s^-2
+
+DAY = 86_400.0
